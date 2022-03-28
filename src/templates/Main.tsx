ed7fac875872313642ea38b37b1fed5debe19ad9
Jofred <jjay.cayabyab@gmail.com>
2022-03-28 12:40:11 -0600

    Add logout button

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 
 import Link from 'next/link';
 
+import { useGlobalContext } from '@/contexts/GlobalContext';
 import { AppConfig } from '@/utils/AppConfig';
 
 type IMainProps = {
@@ -9,63 +10,75 @@ type IMainProps = {
   children: ReactNode;
 };
 
-const Main = (props: IMainProps) => (
-  <div className="w-full px-1 text-gray-700 antialiased">
-    {props.meta}
+const Main = (props: IMainProps) => {
+  const { logout } = useGlobalContext();
 
-    <div className="mx-auto max-w-screen-md">
-      <div className="border-b border-gray-300">
-        <div className="pt-16 pb-8">
-          <div className="text-3xl font-bold text-gray-900">
-            {AppConfig.title}
+  return (
+    <div className="w-full px-1 text-gray-700 antialiased">
+      {props.meta}
+
+      <div className="mx-auto max-w-screen-md">
+        <div className="border-b border-gray-300">
+          <div className="pt-16 pb-8">
+            <div className="text-3xl font-bold text-gray-900">
+              {AppConfig.title}
+            </div>
+            <div className="text-xl">{AppConfig.description}</div>
           </div>
-          <div className="text-xl">{AppConfig.description}</div>
-        </div>
-        <div>
-          <ul className="flex flex-wrap text-xl">
-            <li className="mr-6">
-              <Link href="/">
-                <a className="border-none text-gray-700 hover:text-gray-900">
-                  Home
+          <div>
+            <ul className="flex flex-wrap text-xl">
+              <li className="mr-6">
+                <Link href="/">
+                  <a className="border-none text-gray-700 hover:text-gray-900">
+                    Home
+                  </a>
+                </Link>
+              </li>
+              <li className="mr-6">
+                <Link href="/about/">
+                  <a className="border-none text-gray-700 hover:text-gray-900">
+                    About
+                  </a>
+                </Link>
+              </li>
+              <li className="mr-6">
+                <a
+                  className="border-none text-gray-700 hover:text-gray-900"
+                  href="https://github.com/ixartz/Next-js-Boilerplate"
+                >
+                  GitHub
                 </a>
-              </Link>
-            </li>
-            <li className="mr-6">
-              <Link href="/about/">
-                <a className="border-none text-gray-700 hover:text-gray-900">
-                  About
-                </a>
-              </Link>
-            </li>
-            <li className="mr-6">
-              <a
-                className="border-none text-gray-700 hover:text-gray-900"
-                href="https://github.com/ixartz/Next-js-Boilerplate"
-              >
-                GitHub
-              </a>
-            </li>
-          </ul>
+              </li>
+              <li className="mr-6">
+                <button
+                  className="border-none text-gray-700 hover:text-gray-900"
+                  onClick={logout}
+                >
+                  Logout
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
 
-      <div className="content py-5 text-xl">{props.children}</div>
+        <div className="content py-5 text-xl">{props.children}</div>
 
-      <div className="border-t border-gray-300 py-8 text-center text-sm">
-        © Copyright {new Date().getFullYear()} {AppConfig.title}. Powered with{' '}
-        <span role="img" aria-label="Love">
-          ♥
-        </span>{' '}
-        by <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a>
-        {/*
-         * PLEASE READ THIS SECTION
-         * We'll really appreciate if you could have a link to our website
-         * The link doesn't need to appear on every pages, one link on one page is enough.
-         * Thank you for your support it'll mean a lot for us.
-         */}
+        <div className="border-t border-gray-300 py-8 text-center text-sm">
+          © Copyright {new Date().getFullYear()} {AppConfig.title}. Powered with{' '}
+          <span role="img" aria-label="Love">
+            ♥
+          </span>{' '}
+          by <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a>
+          {/*
+           * PLEASE READ THIS SECTION
+           * We'll really appreciate if you could have a link to our website
+           * The link doesn't need to appear on every pages, one link on one page is enough.
+           * Thank you for your support it'll mean a lot for us.
+           */}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export { Main };
