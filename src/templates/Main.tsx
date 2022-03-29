a4322f87cf80fe47e74523594e2ef826d6689d70
Jofred <jjay.cayabyab@gmail.com>
2022-03-28 18:05:34 -0600

    Add elementary navigation bar

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -1,8 +1,6 @@
 import { ReactNode } from 'react';
 
-import Link from 'next/link';
-
-import { useGlobalContext } from '@/contexts/GlobalContext';
+import Navbar from '@/components/Navbar';
 import { AppConfig } from '@/utils/AppConfig';
 
 type IMainProps = {
@@ -11,56 +9,12 @@ type IMainProps = {
 };
 
 const Main = (props: IMainProps) => {
-  const { logout } = useGlobalContext();
-
   return (
-    <div className="w-full px-1 text-gray-700 antialiased">
+    <div className="w-full text-gray-700 antialiased">
       {props.meta}
 
+      <Navbar />
       <div className="mx-auto max-w-screen-md">
-        <div className="border-b border-gray-300">
-          <div className="pt-16 pb-8">
-            <div className="text-3xl font-bold text-gray-900">
-              {AppConfig.title}
-            </div>
-            <div className="text-xl">{AppConfig.description}</div>
-          </div>
-          <div>
-            <ul className="flex flex-wrap text-xl">
-              <li className="mr-6">
-                <Link href="/">
-                  <a className="border-none text-gray-700 hover:text-gray-900">
-                    Home
-                  </a>
-                </Link>
-              </li>
-              <li className="mr-6">
-                <Link href="/about/">
-                  <a className="border-none text-gray-700 hover:text-gray-900">
-                    About
-                  </a>
-                </Link>
-              </li>
-              <li className="mr-6">
-                <a
-                  className="border-none text-gray-700 hover:text-gray-900"
-                  href="https://github.com/ixartz/Next-js-Boilerplate"
-                >
-                  GitHub
-                </a>
-              </li>
-              <li className="mr-6">
-                <button
-                  className="border-none text-gray-700 hover:text-gray-900"
-                  onClick={logout}
-                >
-                  Logout
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
-
         <div className="content py-5 text-xl">{props.children}</div>
 
         <div className="border-t border-gray-300 py-8 text-center text-sm">
--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,5 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'OneLink',
+  title: 'OneLink',
   locale: 'en',
 };
