680bf48c7f203afe08abd7f0550ea468530a4c03
Navjot Brar <navb2009@hotmail.com>
2022-03-29 12:15:31 -0600

    Add OneLink menu options

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,22 @@
 import React, { Fragment } from 'react';
 
 import { Menu, Transition } from '@headlessui/react';
-import { LogoutIcon } from '@heroicons/react/solid';
-import Link from 'next/link';
+import { LinkIcon, LogoutIcon, PencilAltIcon } from '@heroicons/react/solid';
+import Link, { LinkProps } from 'next/link';
 
 import { useGlobalContext } from '@/contexts/GlobalContext';
 import { AppConfig } from '@/utils/AppConfig';
 
 import SearchBar from './SearchBar';
+
+const NextLink: React.FC<LinkProps> = (props) => {
+  const { href, children, ...rest } = props;
+  return (
+    <Link href={href}>
+      <a {...rest}>{children}</a>
+    </Link>
+  );
+};
 
 export default function Navbar() {
   const { user, logout } = useGlobalContext();
@@ -39,8 +48,42 @@ export default function Navbar() {
               leaveFrom="transform opacity-100 scale-100"
               leaveTo="transform opacity-0 scale-95"
             >
-              <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
-                <div className="p-1 ">
+              <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-300 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                <div className="p-1">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <NextLink href="/">
+                        <a
+                          className={`${
+                            active
+                              ? 'bg-purple-900 text-white'
+                              : 'text-gray-900'
+                          } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                        >
+                          <LinkIcon className="mr-2 h-5 w-5" />
+                          Your OneLink
+                        </a>
+                      </NextLink>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <NextLink href="/edit">
+                        <a
+                          className={`${
+                            active
+                              ? 'bg-purple-900 text-white'
+                              : 'text-gray-900'
+                          } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                        >
+                          <PencilAltIcon className="mr-2 h-5 w-5" />
+                          Edit OneLink
+                        </a>
+                      </NextLink>
+                    )}
+                  </Menu.Item>
+                </div>
+                <div className="p-1">
                   <Menu.Item>
                     {({ active }) => (
                       <button
