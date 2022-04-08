0aed6393c84b3813e3da10a9401606683274e16b
Jofred <jjay.cayabyab@gmail.com>
2022-04-08 12:46:55 -0600

    Add error handling to search

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -4,6 +4,12 @@ import db from './firebaseHandler';
 export default async function handler(req, res) {
   const oneLink = req.body.query;
   const userInfo = await getDoc(doc(db, 'users', oneLink));
+
+  if (!userInfo.data()) {
+    res.status(200).send({ users: [] });
+    return;
+  }
+
   // TODO: Add better searching
   const likes = await getDocs(collection(db, `users/${oneLink}/likes`));
   const links = await getDocs(collection(db, `users/${oneLink}/links`));
@@ -18,5 +24,5 @@ export default async function handler(req, res) {
       link: x.data().link,
     })),
   };
-  res.status(200).json(user);
+  res.status(200).json({ users: [user] });
 }
